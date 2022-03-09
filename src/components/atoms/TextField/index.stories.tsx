33ed5b5acaa38753c27b9cd38dbeef044d255cc9
2g4423 <yoshifumi.tsuji.biz@gmail.com>
2022-03-09 22:53:00 +0900

    Add react-hook-form and yup

--- src/components/atoms/TextField/index.stories.tsx
+++ src/components/atoms/TextField/index.stories.tsx
@@ -1,5 +1,10 @@
 import TextField from './index';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { FormProvider, useForm } from 'react-hook-form';
+import { Button } from '@mui/material';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { action } from '@storybook/addon-actions';
 
 export default {
   title: 'atoms/TextField',
@@ -7,12 +12,30 @@ export default {
 } as ComponentMeta<typeof TextField>;
 
 const Template: ComponentStory<typeof TextField> = (args) => {
-  return <TextField {...args} />;
+  const schema = yup
+    .object()
+    .shape({
+      textField: yup.string().required()
+    })
+    .required();
+  const formMethods = useForm({
+    resolver: yupResolver(schema),
+    defaultValues: { textField: 'Text Field Value' }
+  });
+  return (
+    <FormProvider {...formMethods}>
+      <form onSubmit={formMethods.handleSubmit(action('submit'), action('error'))}>
+        <TextField {...args} />
+        <Button type='submit'>Submit</Button>
+      </form>
+    </FormProvider>
+  );
 };
 
 export const Default = Template.bind({});
 Default.args = {
   name: 'textField',
+  label: 'Text Field',
   size: 'small',
   fullWidth: true
 };
--- src/components/atoms/TextField/index.tsx
+++ src/components/atoms/TextField/index.tsx
@@ -1,13 +1,42 @@
 import MuiTextField from '@mui/material/TextField';
+import { Controller, useFormContext } from 'react-hook-form';
+import _get from 'lodash/get';
+import { FormHelperText } from '@mui/material';
 
 export interface Props {
   name: string;
+  label?: string;
   size?: 'small' | 'medium';
   fullWidth?: boolean;
 }
 
-const TextField = ({ name, size = 'small', fullWidth = false }: Props) => {
-  return <MuiTextField name={name} size={size} fullWidth={fullWidth} />;
+const TextField = ({ name, label, size = 'small', fullWidth = false }: Props) => {
+  const {
+    control,
+    getValues,
+    formState: { errors }
+  } = useFormContext();
+  const errorMessage = _get(errors, `${name}.message`);
+  return (
+    <Controller
+      name={name}
+      control={control}
+      defaultValue={getValues(name)}
+      render={({ field }) => (
+        <>
+          <MuiTextField
+            {...field}
+            name={name}
+            label={label}
+            size={size}
+            fullWidth={fullWidth}
+            error={!!errorMessage}
+          />
+          <FormHelperText error={!!errorMessage}>{errorMessage}</FormHelperText>
+        </>
+      )}
+    />
+  );
 };
 
 export default TextField;
